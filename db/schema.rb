e55bccda08683bf987ff6c65b6376956f38589f2
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2021-01-21 22:45:42 -0500

    Add all funtion of login register and forgot pasword

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_21_214651) do
+ActiveRecord::Schema.define(version: 2021_01_21_225420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,11 @@ ActiveRecord::Schema.define(version: 2021_01_21_214651) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"

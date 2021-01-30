6ecfa108fd1094e7e28f5ee6c655d90ae21841a4
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2021-01-29 20:41:55 -0500

    fix linters

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,7 @@
 module ArticlesHelper
   def count_type(article, count)
     return if article.nil?
+    
     category = content_tag :h4, (link_to article.category.name, article, class: 'yellow_color font-weight-bolder')
     title = content_tag :h5, (link_to article.title, article, class: 'bg_color_title font-weight-bolder')
     content = content_tag :p, truncate(strip_tags(article.body), length: 170)

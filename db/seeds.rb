8f9d2b8558d4beaab02ed29ee4b1eb835ae003b3
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2021-02-02 18:43:42 -0500

    fix heroku deploy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ 
+ Category.create([{ name: 'Sport Bikes', display_in_navbar: true }, {  name: 'Off-road', display_in_navbar: false }])
+

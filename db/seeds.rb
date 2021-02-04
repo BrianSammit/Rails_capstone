cd82678c95585cd263a0bc64712d43e8967311b4
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2021-02-04 18:02:00 -0500

    fix categories with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create([{ name: 'Sport bike', display_in_navbar: true }, { name: 'off-road', display_in_navbar: false },
+    { name: 'Standard', display_in_navbar: true }, { name: 'Sport Touring', display_in_navbar: false },
+    { name: 'Cruiser', display_in_navbar: true }, { name: 'Dual Sport', display_in_navbar: false },
+    { name: 'Touring', display_in_navbar: true }, { name: 'Moped', display_in_navbar: false }])

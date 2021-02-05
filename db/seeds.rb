439a008de0a94c9c521424a13c6a92c2bc0f8029
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2021-02-05 11:35:08 -0500

    Fix error and adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,56 @@ Category.create([{ name: 'Sport bike', display_in_navbar: true }, { name: 'off-r
     { name: 'Cruiser', display_in_navbar: true }, { name: 'Dual Sport', display_in_navbar: false },
     { name: 'Touring', display_in_navbar: true }, { name: 'Moped', display_in_navbar: false }])
 
-    User.create(name: 'Brian', username: 'brian97', email: 'briand@gmail.com', password: '123456', password_confirmation: '123456')
+    User.create(name: 'Brian', username: 'brian97', email: 'brian@gmail.com', password: '123456', password_confirmation: '123456')
     User.create(name: 'Natlia', username: 'nata', email: 'nata@gmail.com', password: '123456', password_confirmation: '123456')
 
 
-# 7.times do |_i|
-#     article = Article.create!(
-#       title: 'Motorcycle', 
-#       body: Faker::Lorem.paragraph_by_chars(number: 500),
-#       user_id: User.first.id, 
-#       category_id: '1',
-#     )
-#     # article.image.attach(io: File.open('/app/assets/images/seed/'), filename: 'r1.pdf', content_type: 'image/png') 
-# end
+7.times do |_i|
+    article = Article.create!(
+      title: 'sports bikes', 
+      body: Faker::Lorem.paragraph_by_chars(number: 500),
+      user_id: User.first.id, 
+      category_id: '1',
+    )
+end
+
+
+5.times do |_i|
+    article = Article.create!(
+      title: 'standar bikes', 
+      body: Faker::Lorem.paragraph_by_chars(number: 500),
+      user_id: User.first.id, 
+      category_id: '2',
+    )
+end
+
+3.times do |_i|
+    article = Article.create!(
+      title: 'cruiser', 
+      body: Faker::Lorem.paragraph_by_chars(number: 500),
+      user_id: User.first.id, 
+      category_id: '3',
+    )
+end
+
+
+5.times do |_i|
+    article = Article.create!(
+      title: 'touring', 
+      body: Faker::Lorem.paragraph_by_chars(number: 500),
+      user_id: User.first.id, 
+      category_id: '4',
+    )
+end
+
+2.times do |_i|
+    article = Article.create!(
+      title: 'Moto', 
+      body: Faker::Lorem.paragraph_by_chars(number: 500),
+      user_id: User.first.id, 
+      category_id: '5',
+    )
+end
+
 
 
